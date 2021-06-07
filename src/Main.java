baa8c63a44385bba02ab61f94f2be2b1c0cf5c14
Andreas Valtersson <Andreas_valter@hotmail.com>
2021-06-07 11:48:40 +0200

    Updates here

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+     static int num =0;
+    public static void main(String[] args) {
+
+    addOne();
+
+        System.out.println(num);
+
+    }
+    public static int addOne (){
+        num++;
+                return num;
+    }
+}

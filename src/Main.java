e2d33cdfe391471ca4e550989af8e43c5f31afb0
Andreas Valtersson <Andreas_valter@hotmail.com>
2021-06-07 11:52:53 +0200

    Change integer num

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
-     static int num =0;
+     static int num =5;
     public static void main(String[] args) {
 
     addOne();
@@ -9,6 +9,7 @@ public class Main {
 
     }
     public static int addOne (){
+        num++;
         num++;
                 return num;
     }
